4b24d53742f5acb6861191d25dec7eca11af78d7
bezawitassaye <beza1221love@gmail.com>
2025-10-19 07:42:46 +0300

    update

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
-
-import { Button } from '.';
+import Button from './Button'; // ✅ direct import
 import { userProfileData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 import avatar from '../data/avatar.jpg';
@@ -21,6 +20,7 @@ const UserProfile = () => {
           borderRadius="50%"
         />
       </div>
+
       <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
         <img
           className="rounded-full h-24 w-24"
@@ -28,29 +28,42 @@ const UserProfile = () => {
           alt="user-profile"
         />
         <div>
-          <p className="font-semibold text-xl dark:text-gray-200"> Michael Roberts </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
-          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@shop.com </p>
+          <p className="font-semibold text-xl dark:text-gray-200">
+            Michael Roberts
+          </p>
+          <p className="text-gray-500 text-sm dark:text-gray-400">
+            Administrator
+          </p>
+          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400">
+            info@shop.com
+          </p>
         </div>
       </div>
+
       <div>
         {userProfileData.map((item, index) => (
-          <div key={index} className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
+          <div
+            key={index}
+            className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer dark:hover:bg-[#42464D]"
+          >
             <button
               type="button"
               style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-              className=" text-xl rounded-lg p-3 hover:bg-light-gray"
+              className="text-xl rounded-lg p-3 hover:bg-light-gray"
             >
               {item.icon}
             </button>
 
             <div>
-              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.desc} </p>
+              <p className="font-semibold dark:text-gray-200">{item.title}</p>
+              <p className="text-gray-500 text-sm dark:text-gray-400">
+                {item.desc}
+              </p>
             </div>
           </div>
         ))}
       </div>
+
       <div className="mt-5">
         <Button
           color="white"
@@ -61,7 +74,6 @@ const UserProfile = () => {
         />
       </div>
     </div>
-
   );
 };
 
